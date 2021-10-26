0bc2f3a1b6e861e0c90bb95ccb0501f5e1599d04
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-10-26 09:48:22 -0500

    Configuración inicial del proyecto móvil VINILOS

---
+++ app/src/main/java/com/miso/vinilos/features/home/ui/views/SplashScreenActivity.kt
@@ -0,0 +1,12 @@
+package com.miso.vinilos.features.home.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miso.vinilos.R
+
+class SplashScreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+    }
+}
