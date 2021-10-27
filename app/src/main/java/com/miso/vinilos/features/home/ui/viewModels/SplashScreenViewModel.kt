61f2a33017ec4e2d62bd555c4d2fdf813769bdad
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-10-26 23:20:46 -0500

    Se creao la clase correspondiente del viewModel para conectar la lógica con la interfaz gráfica de la aplicación

--- app/src/main/java/com/miso/vinilos/features/home/ui/viewModels/SplashScreenViewModel.kt
+++ app/src/main/java/com/miso/vinilos/features/home/ui/viewModels/SplashScreenViewModel.kt
@@ -1,12 +1,14 @@
 package com.miso.vinilos.features.home.ui.viewModels
 
+import androidx.lifecycle.ViewModel
+
 /****
  * Project: vinilos
  * From: com.miso.vinilos.features.home.ui.viewModels
  * Created by Jhonnatan E. Zamudio P. on 26/10/2021 at 10:50 p. m.
- * More info:  https://venecambios-kalunga.com/
  * All rights reserved 2021.
  ****/
 
-class SplashScreenViewModel {
+class SplashScreenViewModel: ViewModel() {
+
 }
--- app/src/main/java/com/miso/vinilos/features/home/ui/views/SplashScreenActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/home/ui/views/SplashScreenActivity.kt
@@ -2,12 +2,28 @@ package com.miso.vinilos.features.home.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.ViewModelProvider
 import com.miso.vinilos.R
+import com.miso.vinilos.databinding.ActivitySplashScreenBinding
+import com.miso.vinilos.features.home.ui.viewModels.SplashScreenViewModel
+import com.miso.vinilos.features.home.ui.viewModels.SplashScreenViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
 
+
+@DelicateCoroutinesApi
 class SplashScreenActivity : AppCompatActivity() {
+
+    private lateinit var viewModel: SplashScreenViewModel
+    private lateinit var binding: ActivitySplashScreenBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         setTheme(R.style.Theme_Vinilos)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash_screen)
+        val viewModelFactory = SplashScreenViewModelFactory.getInstance()
+        viewModel = ViewModelProvider(this, viewModelFactory) [SplashScreenViewModel::class.java]
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 }
