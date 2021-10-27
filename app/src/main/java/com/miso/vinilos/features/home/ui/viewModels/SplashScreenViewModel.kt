f272b232f0b7c5c2384d838e35c1229ec19cffd2
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-10-26 22:51:04 -0500

    Se realizo la interfaz grafica del componente splashScreen

---
+++ app/src/main/java/com/miso/vinilos/features/home/ui/viewModels/SplashScreenViewModel.kt
@@ -0,0 +1,12 @@
+package com.miso.vinilos.features.home.ui.viewModels
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.home.ui.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 26/10/2021 at 10:50 p. m.
+ * More info:  https://venecambios-kalunga.com/
+ * All rights reserved 2021.
+ ****/
+
+class SplashScreenViewModel {
+}
