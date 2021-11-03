b79ffb9354851d72a1cb3d814036191440318748
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-03 12:31:27 -0500

    Se agrega el DashboardActivity.kt con el fin de realizar la transición desde la configuracion inicial

--- app/src/main/java/com/miso/vinilos/features/home/ui/views/InitialSetupActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/home/ui/views/InitialSetupActivity.kt
@@ -1,11 +1,14 @@
 package com.miso.vinilos.features.home.ui.views
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.miso.vinilos.R
+import com.miso.vinilos.core.utils.SharedPreferences
 import com.miso.vinilos.databinding.ActivityInitialSetupBinding
+import com.miso.vinilos.features.home.domain.models.enumerations.KeyPreferences
 import com.miso.vinilos.features.home.ui.viewModels.InitialSetupViewModel
 import com.miso.vinilos.features.home.ui.viewModels.InitialSetupViewModelFactory
 import com.miso.vinilos.features.home.ui.viewModels.SplashScreenViewModel
@@ -25,5 +28,16 @@ class InitialSetupActivity : AppCompatActivity() {
         binding = DataBindingUtil.setContentView(this, R.layout.activity_initial_setup)
         binding.lifecycleOwner = this
         binding.vModel = viewModel
+        viewModel.typeUser.observe(this, {
+            SharedPreferences().setData(this,KeyPreferences.TYPE.value,it)
+            goToDashboard()
+        })
+    }
+
+    private fun goToDashboard() {
+        val intent = Intent(this@InitialSetupActivity, DashboardActivity::class.java)
+        startActivity(intent)
+        //overridePendingTransition(R.anim.left_in, R.anim.left_out)
+        finish()
     }
 }
