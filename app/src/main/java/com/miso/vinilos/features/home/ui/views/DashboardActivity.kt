2a992f1bcb5f4d395640a853e56954aa60700012
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-03 20:48:37 -0500

    Se configuran los menus de navegación con sus respectivos iconos

--- app/src/main/java/com/miso/vinilos/features/home/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/home/ui/views/DashboardActivity.kt
@@ -3,8 +3,10 @@ package com.miso.vinilos.features.home.ui.views
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
 import com.miso.vinilos.R
 import com.miso.vinilos.databinding.ActivityDashboardBinding
+import com.miso.vinilos.features.home.domain.models.enumerations.Navigator
 
 class DashboardActivity : AppCompatActivity() {
 
@@ -14,6 +16,10 @@ class DashboardActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
         binding.lifecycleOwner = this
-        //binding.bottomNav.add()
+        binding.bottomNav.add(MeowBottomNavigation.Model(Navigator.ALBUM.code,R.drawable.ic_album))
+        binding.bottomNav.add(MeowBottomNavigation.Model(Navigator.ARTIST.code,R.drawable.ic_artist))
+        binding.bottomNav.add(MeowBottomNavigation.Model(Navigator.COLLECTOR.code,R.drawable.ic_collector))
+
+        binding.bottomNav.show(Navigator.ALBUM.code, true)
     }
 }
