bd13963a86c3e43bfacb5c5d0e9c2b327d146537
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-03 20:26:30 -0500

    Se implementa el navigationDrawer

--- app/src/main/java/com/miso/vinilos/features/home/ui/views/DashboardActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/home/ui/views/DashboardActivity.kt
@@ -2,11 +2,19 @@ package com.miso.vinilos.features.home.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.miso.vinilos.R
+import com.miso.vinilos.databinding.ActivityDashboardBinding
+import com.miso.vinilos.databinding.ActivityInitialSetupBinding
 
 class DashboardActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityDashboardBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_dashboard)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_dashboard)
+        binding.lifecycleOwner = this
+        //binding.bottomNav.add()
     }
 }
