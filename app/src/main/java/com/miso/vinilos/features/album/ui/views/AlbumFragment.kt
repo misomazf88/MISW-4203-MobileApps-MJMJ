e2ce4e4b6af34938e033f3beb70fa3c093238cb0
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-04 14:41:40 -0500

    Se configura correctamente el adaptador en el AlbumFragment.kt

--- app/src/main/java/com/miso/vinilos/features/album/ui/views/AlbumFragment.kt
+++ app/src/main/java/com/miso/vinilos/features/album/ui/views/AlbumFragment.kt
@@ -8,9 +8,11 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.miso.vinilos.R
-import com.miso.vinilos.databinding.ActivityInitialSetupBinding
 import com.miso.vinilos.databinding.AlbumFragmentBinding
 import com.miso.vinilos.features.album.ui.viewModels.AlbumViewModel
+import androidx.recyclerview.widget.GridLayoutManager
+import com.miso.vinilos.features.album.ui.adapters.AlbumAdapter
+
 
 class AlbumFragment : Fragment() {
 
@@ -20,12 +22,15 @@ class AlbumFragment : Fragment() {
 
     private lateinit var viewModel: AlbumViewModel
     private lateinit var binding: AlbumFragmentBinding
+    private val rvAlbumAdapter = AlbumAdapter()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         viewModel = ViewModelProvider(this).get(AlbumViewModel::class.java)
         binding = DataBindingUtil.inflate(inflater, R.layout.album_fragment,container,false)
         binding.lifecycleOwner = this
         binding.vModel = viewModel
+        binding.rvAlbumes.layoutManager = GridLayoutManager(context,2,GridLayoutManager.VERTICAL,false)
+        binding.rvAlbumes.adapter = rvAlbumAdapter
         return binding.root
     }
 
