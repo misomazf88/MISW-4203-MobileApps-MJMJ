eba2c4a2f37f8993e5117ffad9efc1e401f6e52b
Milena Castillo <sm.castillo@uniandes.edu.co>
2021-11-05 08:36:09 -0500

    Se crean los dataClass para mapear los datos que son enviados por el API Rest para poder ser utilizados en la aplicación móvil

---
+++ app/src/main/java/com/miso/vinilos/features/album/data/entities/ResponseAlbums.kt
@@ -0,0 +1,23 @@
+package com.miso.vinilos.features.album.data.entities
+
+import com.google.gson.annotations.SerializedName
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.album.data.entities
+ * Created by Milena Castillo on 5/11/2021 at 7:46 a. m.
+ * All rights reserved 2021.
+ ****/
+
+data class ResponseAlbums(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("cover") val cover: String,
+    @SerializedName("releaseDate") val releaseDate: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("genre") val genre: String,
+    @SerializedName("recordLabel") val recordLabel: String,
+    @SerializedName("tracks") val tracks: List<Track>,
+    @SerializedName("performers") val performers: List<Performer>,
+    @SerializedName("comments") val comments: List<Comment>
+)
