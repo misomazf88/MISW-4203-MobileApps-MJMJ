8a2b0238048c6b4afb3a080a661f0dd02d6edc09
Juan Jose Chiroque <j.chiroque@uniandes.edu.co>
2021-11-06 11:49:57 -0500

    Se implementa el test para navegación de pestañas

--- app/src/androidTest/java/com/miso/vinilos/features/album/ui/views/AlbumFragmentTest.kt
+++ app/src/androidTest/java/com/miso/vinilos/features/album/ui/views/AlbumFragmentTest.kt
@@ -1,5 +1,7 @@
 package com.miso.vinilos.features.album.ui.views
 
+import android.view.InputDevice
+import android.view.MotionEvent
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import com.miso.vinilos.features.home.ui.views.SplashScreenActivity
@@ -7,12 +9,16 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.junit.Rule
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
 import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.rule.GrantPermissionRule
+import com.miso.vinilos.R
 import org.hamcrest.Matchers.allOf
+
+import androidx.test.espresso.ViewAction
+import androidx.test.espresso.action.*
+
 
 /****
  * Project: vinilos
@@ -40,13 +46,60 @@ class AlbumFragmentTest {
 
     @Test
     fun goToDashboard() {
-        val appCompatButton = onView(
+        val buttonVisitor = onView(
             allOf(
-                ViewMatchers.withId(com.miso.vinilos.R.id.buttonVisitor),
-                ViewMatchers.withText("Visitante"),
+                withId(com.miso.vinilos.R.id.buttonVisitor),
+                withText("Visitante"),
                 isDisplayed()
             )
         )
-        appCompatButton.perform(ViewActions.click())
+        buttonVisitor.perform(ViewActions.click())
     }
+
+    @Test
+    fun navigateBetweenTabs() {
+        val buttonVisitor = onView(
+            allOf(
+                withId(R.id.buttonVisitor),
+                withText("Visitante"),
+                isDisplayed()
+            )
+        )
+        buttonVisitor.perform(ViewActions.click())
+        val viewArtist = onView(allOf(
+            withId(R.id.bottom_nav),
+            isDisplayed()
+        ))
+            viewArtist.perform(clickXY(0.5f,0.5f))
+        val viewCollector = onView(allOf(
+            withId(R.id.bottom_nav),
+            isDisplayed()
+        ))
+        for(x in 0..10)
+            viewCollector.perform(clickXY(0.8f +(0.01f*x),0.5f))
+    }
+
+}
+
+fun clickXY(pctX: Float, pctY: Float): ViewAction? {
+    println(GeneralLocation.CENTER)
+    return ViewActions.actionWithAssertions(
+        GeneralClickAction(
+            Tap.SINGLE,
+            { view ->
+                val screenPos = IntArray(2)
+                view.getLocationOnScreen(screenPos)
+                val w = view.width
+                val h = view.height
+                val x = (w * pctX)
+                val y = (h * pctY)
+                val screenX = screenPos[0] + x
+                val screenY = screenPos[1] + y
+                floatArrayOf(screenX, screenY)
+            },
+            Press.FINGER,
+            InputDevice.SOURCE_UNKNOWN,
+            MotionEvent.BUTTON_PRIMARY
+        )
+    )
 }
