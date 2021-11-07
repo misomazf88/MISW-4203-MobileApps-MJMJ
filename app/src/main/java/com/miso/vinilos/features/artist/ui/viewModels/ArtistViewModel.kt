00f93e7aa7bfacb85a7849ce7ad68925d92fabf4
Milena Castillo <sm.castillo@uniandes.edu.co>
2021-11-07 17:33:54 -0500

    Se agrega el Factory del viewModel de artista y se configura su inyeccion de dependencias del repositorio que necesita

--- app/src/main/java/com/miso/vinilos/features/artist/ui/viewModels/ArtistViewModel.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/viewModels/ArtistViewModel.kt
@@ -1,6 +1,7 @@
 package com.miso.vinilos.features.artist.ui.viewModels
 
 import androidx.lifecycle.ViewModel
+import com.miso.vinilos.features.artist.data.repository.ArtistRepository
 
-class ArtistViewModel : ViewModel() {
+class ArtistViewModel(artistRepository: ArtistRepository) : ViewModel() {
 }
