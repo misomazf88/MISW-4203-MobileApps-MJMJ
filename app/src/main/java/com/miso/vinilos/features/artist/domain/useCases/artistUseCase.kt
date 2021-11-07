3a8037cbc30f33583c2897f5054820726c8f294b
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-11-07 17:52:38 -0500

    Se implementa en la capa de dominio la clase artistUseCase.kt cuyo objetivo es tener la lógica para el viewModel

---
+++ app/src/main/java/com/miso/vinilos/features/artist/domain/useCases/artistUseCase.kt
@@ -0,0 +1,19 @@
+package com.miso.vinilos.features.artist.domain.useCases
+
+import com.miso.vinilos.features.artist.data.repository.ArtistRepository
+import com.miso.vinilos.features.artist.domain.entities.Artist
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.artist.domain.useCases
+ * Created by Miguel Zapata on 7/11/2021 at 5:50 p. m.
+ * All rights reserved 2021.
+ ****/
+
+class artistUseCase (private val artistRepository: ArtistRepository) {
+
+    suspend fun getArtists(): List<Artist> {
+        return artistRepository.getArtistsRemote()
+    }
+
+}
