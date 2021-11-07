6b4953e357620af65d48a0394e78919eeec9462e
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-11-07 18:23:10 -0500

    Se crea la actividad para visualizar el detalle del artista cuando se de clic sobre la imagen en la pantalla que muestra todos los artistas

---
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
@@ -0,0 +1,12 @@
+package com.miso.vinilos.features.artist.ui.views
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.miso.vinilos.R
+
+class ArtistDetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_artist_detail)
+    }
+}
