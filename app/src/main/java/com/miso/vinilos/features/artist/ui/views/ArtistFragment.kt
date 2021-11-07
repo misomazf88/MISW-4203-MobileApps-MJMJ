9c637d11b7dc2efc7482c3a96d67b957b7d5860c
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-11-07 18:32:50 -0500

    Se agrega la funcionalidad del loading para una mejor experiencia de usuario

--- app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistFragment.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistFragment.kt
@@ -5,8 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
+import android.widget.ImageView
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.lifecycleScope
 import androidx.recyclerview.widget.GridLayoutManager
 import com.miso.vinilos.R
 import com.miso.vinilos.core.utils.MyItemDecoration
@@ -17,6 +20,8 @@ import com.miso.vinilos.features.artist.ui.adapters.ArtistAdapter
 import com.miso.vinilos.features.artist.ui.viewModels.ArtistViewModel
 import com.miso.vinilos.features.artist.ui.viewModels.ArtistViewModelFactory
 import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 @DelicateCoroutinesApi
 class ArtistFragment : Fragment() {
@@ -41,7 +46,20 @@ class ArtistFragment : Fragment() {
             viewModel.setLoading(false)
             binding.rvArtistas.adapter = ArtistAdapter(requireContext(),it)
         })
+        viewModel.loading.observe(viewLifecycleOwner, {
+            lifecycleScope.launch(Dispatchers.IO) {
+                animationLoading(binding.imgLoading, it)
+            }
+        })
         return binding.root
+    }
+
+    private fun animationLoading(loading: ImageView, status: Boolean?) {
+        val animation = if (status == true)
+            R.anim.loading
+        else
+            R.anim.invisible
+        loading.startAnimation(AnimationUtils.loadAnimation(context, animation))
     }
 
 }
