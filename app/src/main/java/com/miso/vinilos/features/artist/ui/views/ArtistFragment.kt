210ab7e89382d9edff57f9eef51dc7f792314995
Milena Castillo <sm.castillo@uniandes.edu.co>
2021-11-07 17:38:57 -0500

    Se configura el binding de la clase ArtistFragment.kt

--- app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistFragment.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.miso.vinilos.R
 import com.miso.vinilos.databinding.ArtistFragmentBinding
@@ -24,9 +25,13 @@ class ArtistFragment : Fragment() {
     private lateinit var binding: ArtistFragmentBinding
 
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         val viewModelFactory = ArtistViewModelFactory.getInstance()
         viewModel = ViewModelProvider(this, viewModelFactory)[ArtistViewModel::class.java]
+        binding = DataBindingUtil.inflate(inflater, R.layout.artist_fragment,container,false)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
+        return binding.root
     }
 
 }
