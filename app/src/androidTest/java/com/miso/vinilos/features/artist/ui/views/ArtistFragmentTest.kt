79088d8a1acbd462697cfa91f7835c7301663fa3
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-07 18:59:44 -0500

    Se crea la clase de test donde se realizaran los escenarios de prueba de la interfaz gráfica

---
+++ app/src/androidTest/java/com/miso/vinilos/features/artist/ui/views/ArtistFragmentTest.kt
@@ -0,0 +1,14 @@
+package com.miso.vinilos.features.artist.ui.views
+
+import org.junit.Assert.*
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.artist.ui.views
+ * Created by Jhonnatan E. Zamudio P. on 7/11/2021 at 6:58 p. m.
+ * All rights reserved 2021.
+ */
+
+class ArtistFragmentTest {
+
+}
