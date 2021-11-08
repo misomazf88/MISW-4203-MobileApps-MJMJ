81b5580794080d8115905dab856357fda7fd1304
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-07 19:10:13 -0500

    Se agrega el primer escenario donde navega hasta la pestaña de artistas

--- app/src/androidTest/java/com/miso/vinilos/features/artist/ui/views/ArtistFragmentTest.kt
+++ app/src/androidTest/java/com/miso/vinilos/features/artist/ui/views/ArtistFragmentTest.kt
@@ -1,11 +1,18 @@
 package com.miso.vinilos.features.artist.ui.views
 
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.filters.LargeTest
 import androidx.test.rule.ActivityTestRule
 import androidx.test.rule.GrantPermissionRule
+import com.miso.vinilos.R
+import com.miso.vinilos.core.ClickScreen
 import com.miso.vinilos.features.home.ui.views.SplashScreenActivity
 import kotlinx.coroutines.DelicateCoroutinesApi
+import org.hamcrest.Matchers
 import org.junit.Rule
+import org.junit.Test
 import org.junit.runner.RunWith
 
 /****
@@ -32,5 +39,23 @@ class ArtistFragmentTest {
             "android.permission.WRITE_EXTERNAL_STORAGE"
         )
 
+    @Test
+    fun goToArtists() {
+        val buttonVisitor = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.buttonCollector),
+                ViewMatchers.withText("Coleccionista"),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        buttonVisitor.perform(ViewActions.click())
+        val viewArtist = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.bottom_nav),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        viewArtist.perform(ClickScreen().XY(0.5f,0.5f))
+    }
 
 }
