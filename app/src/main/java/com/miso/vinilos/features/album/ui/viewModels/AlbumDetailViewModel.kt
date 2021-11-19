4be52e3b548ddb7681658a6ec03a1e6df9386ca7
Jhonnatan Eduardo Zamudio Palacios <j.zamudiop@uniandes.edu.co>
2021-11-19 06:26:18 -0500

    Se agrega el recurso del fondo para mostrar el detalle del album

---
+++ app/src/main/java/com/miso/vinilos/features/album/ui/viewModels/AlbumDetailViewModel.kt
@@ -0,0 +1,13 @@
+package com.miso.vinilos.features.album.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.album.ui.viewModels
+ * Created by Jhonnatan E. Zamudio P. on 18/11/2021 at 9:02 a. m.
+ * All rights reserved 2021.
+ ****/
+
+class AlbumDetailViewModel : ViewModel() {
+}
