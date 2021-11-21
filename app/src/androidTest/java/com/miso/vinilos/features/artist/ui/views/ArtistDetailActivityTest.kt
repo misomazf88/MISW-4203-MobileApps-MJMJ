f9f97699319fef3e759bfb950f24bfccfe69c9bf
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-11-21 16:17:55 -0500

    Se implementa un escenario en donde se accede a la pestaña de artistas y se da clic sobre el para visualizar su detalle y posteriormente se devuelve a la pantalla principal

--- app/src/androidTest/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivityTest.kt
+++ app/src/androidTest/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivityTest.kt
@@ -1,6 +1,23 @@
 package com.miso.vinilos.features.artist.ui.views
 
-import org.junit.Assert.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import com.miso.vinilos.R
+import com.miso.vinilos.core.ClickScreen
+import com.miso.vinilos.features.home.ui.views.SplashScreenActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /****
  * Project: vinilos
@@ -9,6 +26,51 @@ import org.junit.Assert.*
  * All rights reserved 2021.
  */
 
+@DelicateCoroutinesApi
+@LargeTest
+@RunWith(androidx.test.runner.AndroidJUnit4::class)
 class ArtistDetailActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashScreenActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"
+        )
+
+    @Test
+    fun clickToArtistWithAlbums() {
+        val buttonVisitor = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.buttonCollector),
+                ViewMatchers.withText("Coleccionista"),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        buttonVisitor.perform(ViewActions.click())
+        val viewArtist = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.bottom_nav),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        viewArtist.perform(ClickScreen().XY(0.5f,0.5f))
+        Espresso.onView(ViewMatchers.withId(R.id.rvArtistas))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
+                    ViewActions.click()
+                )
+            )
+        runBlocking {
+            delay(3000)
+        }
+        Espresso.pressBack()
+    }
 
 }
