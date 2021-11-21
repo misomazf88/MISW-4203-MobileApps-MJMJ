20ca333ff90386027b2e7eaf539336b137848313
Juan chiroque <j.chiroque@uniandes.edu.co>
2021-11-21 10:49:03 -0500

    Se implementa el viewModel para la vista de detalle del artista

---
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/viewModels/ArtistDetailViewModel.kt
@@ -0,0 +1,14 @@
+package com.miso.vinilos.features.artist.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.artist.ui.viewModels
+ * Created by Juan Chiroque on 21/11/2021 at 10:47 a. m.
+ * All rights reserved 2021.
+ ****/
+
+class ArtistDetailViewModel: ViewModel() {
+
+}
