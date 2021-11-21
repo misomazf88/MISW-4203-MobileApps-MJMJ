e24e4cc08e267af25780fe6581bdc2741f3d53eb
Juan chiroque <j.chiroque@uniandes.edu.co>
2021-11-20 20:33:41 -0500

    Se crea el primer escenario donde se da clic en un album que contiene canciones se espera 3 segundos y se devuelve a la pantalla anterior

--- app/src/androidTest/java/com/miso/vinilos/features/album/ui/views/AlbumDetailActivityTest.kt
+++ app/src/androidTest/java/com/miso/vinilos/features/album/ui/views/AlbumDetailActivityTest.kt
@@ -1,6 +1,22 @@
 package com.miso.vinilos.features.album.ui.views
 
-import org.junit.Assert.*
+import androidx.recyclerview.widget.RecyclerView
+import androidx.test.espresso.Espresso
+import androidx.test.espresso.action.ViewActions
+import androidx.test.espresso.contrib.RecyclerViewActions
+import androidx.test.espresso.matcher.ViewMatchers
+import androidx.test.filters.LargeTest
+import androidx.test.rule.ActivityTestRule
+import androidx.test.rule.GrantPermissionRule
+import com.miso.vinilos.R
+import com.miso.vinilos.features.home.ui.views.SplashScreenActivity
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import org.hamcrest.Matchers
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
 
 /****
  * Project: vinilos
@@ -9,6 +25,44 @@ import org.junit.Assert.*
  * All rights reserved 2021.
  */
 
+@DelicateCoroutinesApi
+@LargeTest
+@RunWith(androidx.test.runner.AndroidJUnit4::class)
 class AlbumDetailActivityTest {
+
+    @Rule
+    @JvmField
+    var mActivityTestRule = ActivityTestRule(SplashScreenActivity::class.java)
+
+    @Rule
+    @JvmField
+    var mGrantPermissionRule =
+        GrantPermissionRule.grant(
+            "android.permission.CAMERA",
+            "android.permission.WRITE_EXTERNAL_STORAGE"
+        )
+
+    @Test
+    fun clickToAlbumWithTracks() {
+        val buttonVisitor = Espresso.onView(
+            Matchers.allOf(
+                ViewMatchers.withId(R.id.buttonVisitor),
+                ViewMatchers.withText("Visitante"),
+                ViewMatchers.isDisplayed()
+            )
+        )
+        buttonVisitor.perform(ViewActions.click())
+        Espresso.onView(ViewMatchers.withId(R.id.rvAlbumes))
+            .perform(
+                RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
+                    0,
+                    ViewActions.click()
+                )
+            )
+        runBlocking {
+            delay(3000)
+        }
+        Espresso.pressBack()
+    }
 
 }
