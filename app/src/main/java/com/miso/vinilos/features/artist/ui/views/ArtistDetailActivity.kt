b8039aa83acd277015111ac98647c7c9ae1565e5
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2021-11-21 15:18:06 -0500

    Se implementa el binding de forma correcta para acceder al xml

--- app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
@@ -2,9 +2,11 @@ package com.miso.vinilos.features.artist.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.miso.vinilos.R
+import com.miso.vinilos.databinding.ActivityArtistDetailBinding
 import com.miso.vinilos.features.artist.domain.entities.Artist
 import com.miso.vinilos.features.artist.ui.viewModels.ArtistDetailViewModel
 import com.miso.vinilos.features.artist.ui.viewModels.ArtistDetailViewModelFactory
@@ -15,13 +17,16 @@ class ArtistDetailActivity : AppCompatActivity() {
 
     val gson = Gson()
     private lateinit var viewModel: ArtistDetailViewModel
+    private lateinit var binding: ActivityArtistDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val artist = gson.fromJson(intent.getStringExtra("artist"), Artist::class.java)
         val viewModelFactory = ArtistDetailViewModelFactory.getInstance()
         viewModel = ViewModelProvider(this, viewModelFactory)[ArtistDetailViewModel::class.java]
-        setContentView(R.layout.activity_artist_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_artist_detail)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 
 }
