e67ae460b1f41b494b8d950733c70bec2cfca53b
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2021-11-21 15:15:55 -0500

    Se implementa el viewModel Factory con el fin de tener MVVM en la aplicación del detalle de cada artista

--- app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
@@ -2,18 +2,25 @@ package com.miso.vinilos.features.artist.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.miso.vinilos.R
 import com.miso.vinilos.features.artist.domain.entities.Artist
+import com.miso.vinilos.features.artist.ui.viewModels.ArtistDetailViewModel
+import com.miso.vinilos.features.artist.ui.viewModels.ArtistDetailViewModelFactory
+import kotlinx.coroutines.DelicateCoroutinesApi
 
+@DelicateCoroutinesApi
 class ArtistDetailActivity : AppCompatActivity() {
 
     val gson = Gson()
+    private lateinit var viewModel: ArtistDetailViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val artist = gson.fromJson(intent.getStringExtra("artist"), Artist::class.java)
-        println("Los datos son: " + artist)
+        val viewModelFactory = ArtistDetailViewModelFactory.getInstance()
+        viewModel = ViewModelProvider(this, viewModelFactory)[ArtistDetailViewModel::class.java]
         setContentView(R.layout.activity_artist_detail)
     }
 
