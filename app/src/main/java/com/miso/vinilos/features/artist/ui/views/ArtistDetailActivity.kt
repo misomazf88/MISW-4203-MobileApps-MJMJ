b112b3b70e2adf20615afab73c3a8c44cf6ca23c
Juan chiroque <j.chiroque@uniandes.edu.co>
2021-11-21 15:05:07 -0500

    Se configura de forma correcta el envio de los datos de la pantalla principal al detalle de cada artista

--- app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/artist/ui/views/ArtistDetailActivity.kt
@@ -2,11 +2,19 @@ package com.miso.vinilos.features.artist.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 import com.miso.vinilos.R
+import com.miso.vinilos.features.artist.domain.entities.Artist
 
 class ArtistDetailActivity : AppCompatActivity() {
+
+    val gson = Gson()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val artist = gson.fromJson(intent.getStringExtra("artist"), Artist::class.java)
+        println("Los datos son: " + artist)
         setContentView(R.layout.activity_artist_detail)
     }
+
 }
