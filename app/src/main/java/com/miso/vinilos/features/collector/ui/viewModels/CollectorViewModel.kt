b5920060eb7c6313320005e92ad92634d47a9fd7
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2021-11-21 20:38:19 -0500

    Se implementa logica para traer los coleccionistas desde el viewModel y posteriormente ser expuesto en el fragment

--- app/src/main/java/com/miso/vinilos/features/collector/ui/viewModels/CollectorViewModel.kt
+++ app/src/main/java/com/miso/vinilos/features/collector/ui/viewModels/CollectorViewModel.kt
@@ -3,10 +3,13 @@ package com.miso.vinilos.features.collector.ui.viewModels
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.miso.vinilos.features.collector.data.repository.CollectorRepository
 import com.miso.vinilos.features.collector.domain.entities.Collector
 import com.miso.vinilos.features.collector.domain.useCases.CollectorUseCase
+import kotlinx.coroutines.*
 
+@DelicateCoroutinesApi
 class CollectorViewModel(collectorRepository: CollectorRepository) : ViewModel() {
 
     private val collectorUseCase = CollectorUseCase(collectorRepository)
@@ -17,6 +20,21 @@ class CollectorViewModel(collectorRepository: CollectorRepository) : ViewModel()
 
     init {
         setLoading(true)
+        GlobalScope.launch {
+            withContext(Dispatchers.IO) {
+                getCollectors()
+            }
+        }
+    }
+
+    private fun getCollectors() {
+        viewModelScope.launch {
+            setCollectors(collectorUseCase.getCollectors())
+        }
+    }
+
+    private fun setCollectors(collectors: List<Collector>) {
+        _collectors.value = collectors
     }
 
     fun setLoading(status: Boolean) {
