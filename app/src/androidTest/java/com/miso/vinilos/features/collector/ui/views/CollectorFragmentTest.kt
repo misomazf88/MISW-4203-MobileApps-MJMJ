ab1fb7481794c62acc2076900130f421a764b3d3
Milena Castillo <sm.castillo@uniandes.edu.co>
2021-11-21 21:33:32 -0500

    Se crea la clase para realizar el test de forma automatica

---
+++ app/src/androidTest/java/com/miso/vinilos/features/collector/ui/views/CollectorFragmentTest.kt
@@ -0,0 +1,14 @@
+package com.miso.vinilos.features.collector.ui.views
+
+import org.junit.Assert.*
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.collector.ui.views
+ * Created by Milena Castillo on 21/11/2021 at 9:33 p. m.
+ * All rights reserved 2021.
+ */
+
+class CollectorFragmentTest {
+
+}
