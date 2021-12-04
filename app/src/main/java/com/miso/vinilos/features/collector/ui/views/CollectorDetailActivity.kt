d007cc646f309d5281fc9fb1b47b4aebf73e25f0
Jhonnatan Zamudio <j.zamudiop@uniandes.edu.co>
2021-12-04 18:32:03 -0500

    Se agrega la captura de los datos del coleccionista para ser mostrado en el detalle

--- app/src/main/java/com/miso/vinilos/features/collector/ui/views/CollectorDetailActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/collector/ui/views/CollectorDetailActivity.kt
@@ -2,11 +2,19 @@ package com.miso.vinilos.features.collector.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.gson.Gson
 import com.miso.vinilos.R
+import com.miso.vinilos.features.artist.domain.entities.Artist
+import com.miso.vinilos.features.collector.domain.entities.Collector
 
 class CollectorDetailActivity : AppCompatActivity() {
+
+    val gson = Gson()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val collector = gson.fromJson(intent.getStringExtra("collector"), Collector::class.java)
         setContentView(R.layout.activity_collector_detail)
+        println("El coleccionista es: " + collector)
     }
 }
