77c8fadcf082e64b8f35357cee32ad11895163d3
Milena Castillo <sm.castillo@uniandes.edu.co>
2021-12-04 19:03:32 -0500

    Se configura el binding de la pantalla detalle para poder mostrar la información de forma correcta

--- app/src/main/java/com/miso/vinilos/features/collector/ui/views/CollectorDetailActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/collector/ui/views/CollectorDetailActivity.kt
@@ -2,10 +2,11 @@ package com.miso.vinilos.features.collector.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.google.gson.Gson
 import com.miso.vinilos.R
-import com.miso.vinilos.features.artist.ui.viewModels.ArtistDetailViewModel
+import com.miso.vinilos.databinding.ActivityCollectorDetailBinding
 import com.miso.vinilos.features.collector.domain.entities.Collector
 import com.miso.vinilos.features.collector.ui.viewModels.CollectorDetailViewModel
 import com.miso.vinilos.features.collector.ui.viewModels.CollectorDetailViewModelFactory
@@ -16,12 +17,15 @@ class CollectorDetailActivity : AppCompatActivity() {
 
     val gson = Gson()
     private lateinit var viewModel: CollectorDetailViewModel
+    private lateinit var binding: ActivityCollectorDetailBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val collector = gson.fromJson(intent.getStringExtra("collector"), Collector::class.java)
         val viewModelFactory = CollectorDetailViewModelFactory.getInstance()
         viewModel = ViewModelProvider(this, viewModelFactory)[CollectorDetailViewModel::class.java]
-        setContentView(R.layout.activity_collector_detail)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_collector_detail)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 }
