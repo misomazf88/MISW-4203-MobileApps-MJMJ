2f3c7ed82a2f0db842ba7bee05ff823a7178ed50
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-12-05 18:57:22 -0500

    Se configura el binding para relacionar el activity con el xml de la pantalla crear álbum

--- app/src/main/java/com/miso/vinilos/features/album/ui/views/CreateAlbumActivity.kt
+++ app/src/main/java/com/miso/vinilos/features/album/ui/views/CreateAlbumActivity.kt
@@ -2,8 +2,10 @@ package com.miso.vinilos.features.album.ui.views
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
 import com.miso.vinilos.R
+import com.miso.vinilos.databinding.ActivityCreateAlbumBinding
 import com.miso.vinilos.features.album.ui.viewModels.AlbumCreateViewModel
 import com.miso.vinilos.features.album.ui.viewModels.AlbumCreateViewModelFactory
 import kotlinx.coroutines.DelicateCoroutinesApi
@@ -12,11 +14,14 @@ import kotlinx.coroutines.DelicateCoroutinesApi
 class CreateAlbumActivity : AppCompatActivity() {
 
     private lateinit var viewModel: AlbumCreateViewModel
+    private lateinit var binding: ActivityCreateAlbumBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val viewModelFactory = AlbumCreateViewModelFactory.getInstance()
         viewModel = ViewModelProvider(this, viewModelFactory)[AlbumCreateViewModel::class.java]
-        setContentView(R.layout.activity_create_album)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_create_album)
+        binding.lifecycleOwner = this
+        binding.vModel = viewModel
     }
 }
