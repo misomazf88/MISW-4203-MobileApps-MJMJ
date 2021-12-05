08f8ed42c646d92be435959450c328da97d8129e
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-12-04 22:38:11 -0500

    Se crea la clase de test para realizar las pruebas de interfaz gráfica automatizada

---
+++ app/src/androidTest/java/com/miso/vinilos/features/collector/ui/views/CollectorDetailActivityTest.kt
@@ -0,0 +1,14 @@
+package com.miso.vinilos.features.collector.ui.views
+
+import org.junit.Assert.*
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.collector.ui.views
+ * Created by Jhonnatan E. Zamudio P. on 4/12/2021 at 10:36 p. m.
+ * All rights reserved 2021.
+ */
+
+class CollectorDetailActivityTest {
+
+}
