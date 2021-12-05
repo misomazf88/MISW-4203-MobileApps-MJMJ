7df364e8469737ed9580b96a6469d09ed9c27878
Miguel Zapata <ma.zapataf@uniandes.edu.co>
2021-12-05 18:25:03 -0500

    Se implementaron todos los elementos básicos para la interfaz visual de crear álbum

---
+++ app/src/main/java/com/miso/vinilos/features/album/ui/viewModels/AlbumCreateViewModel.kt
@@ -0,0 +1,14 @@
+package com.miso.vinilos.features.album.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+
+/****
+ * Project: vinilos
+ * From: com.miso.vinilos.features.album.ui.viewModels
+ * Created by Miguel Zapata on 5/12/2021 at 4:37 p. m.
+ * All rights reserved 2021.
+ ****/
+
+class AlbumCreateViewModel: ViewModel() {
+
+}
